881aac527465701d77f69ba01e0dfd6dc87c0f9e
mikershov94 <ershovme@gmail.com>
2021-04-29 18:36:34 +0800

    Added content to MainPage

--- src/components/Pages/MainPage.jsx
+++ src/components/Pages/MainPage.jsx
@@ -4,8 +4,14 @@ import './Pages.sass';
 
 const MainPage = () => {
     return(
-        <div className="d-flex justify-content-center main-page">
-            <h1>Добро пожаловать</h1>
+        <div className="page-container">
+            <div className="main-page">
+                <h1>Добро пожаловать</h1>
+                <p>Вас приветствует приложение StarDB</p>
+                <p>Погрузитесь в незабываемый мир Star Wars</p>
+                <p>Приложение использует API https://swapi.dev/api и https://starwars-visualguide.com/assets/img/</p>
+                <p>Вы можете переходить по ссылкам в шапке сайта и читать про героев, планеты и корабли вселенной StarWars</p>
+            </div>
         </div>
     );
 }
